5180fe528bd86640bf21160aeb1cc9aad3f7249e
Yura Korniyenko <yura.kornssnko@gmail.com>
2020-11-29 17:26:48 +0200

    PC version completed

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,19 +1,24 @@
 import $ from "jquery";
 
+// <------ Header menu START ------>
 $(".header__menu li").on("click", function () {
   if (!$(this).hasClass("active")) {
     $(".header__menu li").removeClass("active");
     $(this).addClass("active");
   }
 });
+// <------ Header menu END ------>
 
+// <------ Sellers color select START ------>
 $(".sellers__circle").on("click", function () {
   if (!$(this).hasClass("active")) {
     $(".sellers__circle").removeClass("active");
     $(this).addClass("active");
   }
 });
+// <------ Sellers color select END ------>
 
+// <------ Deals timer START ------>
 let timerBlock = $(".deals__slider-timer");
 let time = 24157;
 
@@ -37,3 +42,49 @@ let timerId = setInterval(function () {
     clearInterval(timerId);
   }
 }, 1000);
+// <------ Deals timer END ------>
+
+// <------ Email check START ------>
+let pattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/;
+let mail = $(".newsletter__email");
+let mail_btn = $(".newsletter__submit");
+let label = $("#label");
+
+mail.on("input", function (e) {
+  if (mail.val() !== " ") {
+    if (mail.val().search(pattern) == 0) {
+      mail.removeClass("error");
+      label.removeClass("error");
+      label.addClass("valid");
+      mail_btn.attr("disabled", false);
+    } else {
+      label.removeClass("valid");
+      mail.addClass("error");
+      label.addClass("error");
+      mail_btn.attr("disabled", true);
+    }
+  }
+});
+// <------ Email check END ------>
+
+// <------ Footer email check START ------>
+let footer_mail = $(".footer__email");
+let footer_mail_btn = $(".footer__submit");
+let footer_label = $("#footer__label");
+
+footer_mail.on("input", function (e) {
+  if (footer_mail.val() !== " ") {
+    if (footer_mail.val().search(pattern) == 0) {
+      footer_mail.removeClass("error");
+      footer_label.removeClass("error");
+      footer_label.addClass("valid");
+      footer_mail_btn.attr("disabled", false);
+    } else {
+      footer_label.removeClass("valid");
+      footer_mail.addClass("error");
+      footer_label.addClass("error");
+      footer_mail_btn.attr("disabled", true);
+    }
+  }
+});
+// <------ Footer email check END ------>
